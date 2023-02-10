12ee5ebf94e6625e2da105b9e9ddbac558478f72
kevinalarcon95 <kfalarcon@unicauca.edu.co>
2023-02-09 23:28:33 -0500

    Constructores clase Faculty

--- src/model/Faculty.ts
+++ src/model/Faculty.ts
@@ -1,8 +1,20 @@
 export class Faculty{
     faculties: RespFaculty[] = [];
+
+    constructor(
+        partil?: Partial<Faculty>
+    ){
+        Object.assign(this, partil);
+    }
 }
 
 export class RespFaculty{
     id: number;
     name: string;
+
+    constructor(
+        partil?: Partial<RespFaculty>
+    ){
+        Object.assign(this, partil);
+    }
 }
